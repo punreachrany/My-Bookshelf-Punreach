a92f733046027bb8ed4f06eb311a7d68839f8a17
Punreach Rany <punreachrany@Punreachs-MacBook-Pro.local>
2020-12-19 17:43:56 +0900

    book webview and browser launching

---
+++ lib/screens/detail/book_webview.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class BookWebView extends StatefulWidget {
+  final String title, url;
+  const BookWebView({Key key, this.title, this.url}) : super(key: key);
+  @override
+  _BookWebViewState createState() => _BookWebViewState(title, url);
+}
+
+class _BookWebViewState extends State<BookWebView> {
+  String title, url;
+  _BookWebViewState(this.title, this.url);
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          if (await canLaunch(url)) {
+            await launch(url);
+          } else {
+            // can't launch url, there is some error
+            throw "Could not launch $url";
+          }
+        },
+        child: Icon(Icons.open_in_browser_rounded),
+        backgroundColor: Colors.purple,
+      ),
+      body: WebView(
+        initialUrl: url,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+    );
+  }
+}
